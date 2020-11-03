474082a4971353084cd7f9d980baf11c754bd8a9
Juan Ocho <juaneme8@gmail.com>
2020-11-03 20:04:58 -0300

    Minor changes in sidebar

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -5,11 +5,11 @@ export const SidebarData = [
 		title: 'Resumen',
 		path: '/',
 		icon: (
-			<svg fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+			<svg className='link__icon' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
 				<path
 					strokeLinecap='round'
 					strokeLinejoin='round'
-					strokeWidth='2'
+					strokeWidth='1'
 					d='M9 17V7m0 10a2 2 0 01-2 2H5a2 2 0 01-2-2V7a2 2 0 012-2h2a2 2 0 012 2m0 10a2 2 0 002 2h2a2 2 0 002-2M9 7a2 2 0 012-2h2a2 2 0 012 2m0 10V7m0 10a2 2 0 002 2h2a2 2 0 002-2V7a2 2 0 00-2-2h-2a2 2 0 00-2 2'
 				></path>
 			</svg>
@@ -19,11 +19,11 @@ export const SidebarData = [
 		title: 'Facturación',
 		path: '/facturacion',
 		icon: (
-			<svg fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+			<svg className='link__icon' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
 				<path
 					strokeLinecap='round'
 					strokeLinejoin='round'
-					strokeWidth='2'
+					strokeWidth='1'
 					d='M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z'
 				></path>
 			</svg>
@@ -33,12 +33,12 @@ export const SidebarData = [
 		title: 'Auditor',
 		path: '/auditor',
 		icon: (
-			<svg fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
-				<path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M15 12a3 3 0 11-6 0 3 3 0 016 0z'></path>
+			<svg className='link__icon' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+				<path strokeLinecap='round' strokeLinejoin='round' strokeWidth='1' d='M15 12a3 3 0 11-6 0 3 3 0 016 0z'></path>
 				<path
 					strokeLinecap='round'
 					strokeLinejoin='round'
-					strokeWidth='2'
+					strokeWidth='1'
 					d='M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z'
 				></path>
 			</svg>
@@ -48,8 +48,8 @@ export const SidebarData = [
 		title: 'Analisis',
 		path: '/analisis',
 		icon: (
-			<svg fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
-				<path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M13 7h8m0 0v8m0-8l-8 8-4-4-6 6'></path>
+			<svg className='link__icon' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+				<path strokeLinecap='round' strokeLinejoin='round' strokeWidth='1' d='M13 7h8m0 0v8m0-8l-8 8-4-4-6 6'></path>
 			</svg>
 		),
 	},
@@ -57,11 +57,11 @@ export const SidebarData = [
 		title: 'Control',
 		path: '/control',
 		icon: (
-			<svg fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+			<svg className='link__icon' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
 				<path
 					strokeLinecap='round'
 					strokeLinejoin='round'
-					strokeWidth='2'
+					strokeWidth='1'
 					d='M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4'
 				></path>
 			</svg>
