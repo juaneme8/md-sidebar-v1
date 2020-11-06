f36493a252ab459ca94f3ba042f987d6842a8306
Juan Ocho <juaneme8@gmail.com>
2020-11-06 10:36:51 -0300

    attemps to get access token after getting code

--- src/App.js
+++ src/App.js
@@ -13,16 +13,15 @@ function App() {
 	return (
 		<>
 			<Router>
+				<Route exact path='/callback' component={Callback} />
 				<TopNav />
 				<PageTitle title='Resumen' subtitle='Dashboard' />
 				<Sidebar />
 				<InstantGraphs />
-				<Route exact path='/callback' component={Callback} />
+				<a href='http://api.testing.powermeter.com.ar/o/authorize/?response_type=code&client_id=RDhguCvOW4hVlI0T39tC9jY71Mp3lTOsKf7wAByC&redirect_uri=http://localhost:3000/callback/'>
+					Autorizar
+				</a>
 			</Router>
-
-			<a href='http://api.testing.powermeter.com.ar/o/authorize/?response_type=code&client_id=RDhguCvOW4hVlI0T39tC9jY71Mp3lTOsKf7wAByC&redirect_uri=http://localhost:3000/callback/'>
-				Autorizar
-			</a>
 		</>
 	);
 }
--- src/components/Callback.js
+++ src/components/Callback.js
@@ -1,6 +1,63 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-function Callback() {
+function Callback(props) {
+	// app.get('/callback', function(req, res) {
+	// 	let code = req.query.code || null
+	// 	let authOptions = {
+	// 	  url: 'https://accounts.spotify.com/api/token',
+	// 	  form: {
+	// 		code: code,
+	// 		redirect_uri,
+	// 		grant_type: 'authorization_code'
+	// 	  },
+	// 	  headers: {
+	// 		'Authorization': 'Basic ' + (new Buffer(
+	// 		  process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
+	// 		).toString('base64'))
+	// 	  },
+	// 	  json: true
+	// 	}
+	// 	request.post(authOptions, function(error, response, body) {
+	// 	  var access_token = body.access_token
+	// 	  let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
+	// 	  res.redirect(uri + '?access_token=' + access_token)
+	// 	})
+	//   })
+
+	useEffect(() => {
+		const code = new URLSearchParams(props.location.search).get('code');
+		console.log(code);
+
+		(async () => {
+			const rawResponse = await fetch('http://api.testing.powermeter.com.ar/o/token', {
+				method: 'POST',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/x-www-form-urlencoded,application/x-www-form-urlencoded',
+				},
+				body: JSON.stringify({
+					grant_type: 'authorization_code',
+					code: code,
+					redirect_uri: 'http://localhost:3000/callback',
+					client_id: 'RDhguCvOW4hVlI0T39tC9jY71Mp3lTOsKf7wAByC',
+				}),
+			});
+			const content = await rawResponse.json();
+
+			console.log(content);
+		})();
+
+		fetch(
+			`http://api.testing.powermeter.com.ar/o/token?grant_type=authorization_code&code=${code}&redirect_uri=http://localhost:3000/callback/&client_id=RDhguCvOW4hVlI0T39tC9jY71Mp3lTOsKf7wAByC`,
+			{
+				method: 'POST',
+			}
+		)
+			.then(response => response.json())
+			.then(data => console.log(data));
+	}, []); //http://api.testing.powermeter.com.ar/o/authorize/?response_type=code&client_id=RDhguCvOW4hVlI0T39tC9jY71Mp3lTOsKf7wAByC&redirect_uri=http://localhost:3000/callback/'
+	//`http://api.testing.powermeter.com.ar/o/token/?client_id=RDhguCvOW4hVlI0T39tC9jY71Mp3lTOsKf7wAByC&grant_type=authorization_code&code=${code}&redirect_uri=http://localhost:3000/callback/`;
+
 	return (
 		<div>
 			<h1>titulos</h1>
